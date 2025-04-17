87a579de9718d557c72ed60bb5fe017936f52a0d
JureFilipovic <jure.filipovic@gmail.com>
2025-04-17 14:35:15 +0200

    Added form validation for all fields.

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 const myLibrary = [];
 
-// function Book (title, author, pages, haveRead) {
-//     this.title = title;
-//     this.author = author;
-//     this.pages = pages;
-//     this.haveRead = haveRead;
-// }
-
 class Book {
     constructor (title, author, pages, haveRead) {
         this.title = title;
